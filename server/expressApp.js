472d53d5836d36eaed36a55fa2c43380eb2599f2
sivaplaysmC <sivaram.asdf@gmail.com>
2024-09-28 18:31:12 +0530

    FEAT: add cookie jwt authentication middleware

--- server/expressApp.js
+++ server/expressApp.js
@@ -1,9 +1,11 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
 import employeeRouter from "./src/route/employee.js";
 import { ErrorHandler } from "./src/middleware/error-handler.js";
 import { currentUser } from "./src/middleware/current-user.js";
 import { requireAuth } from "./src/middleware/require-auth.js";
+
+import cookieParser from "cookie-parser";
 
 import sample from "./src/route/sample.js";
 import login from "./src/route/login.js";
@@ -11,20 +13,23 @@ import login from "./src/route/login.js";
 const app = express();
 
 app.use(express.json());
-
-// app.use(currentUser);
-// app.use(requireAuth);
+app.use(cookieParser());
 
 // app.use(employeeRouter);
 
-
-app.use(employeeRouter)
+app.use(employeeRouter);
 
 app.use("/sample", sample);
 app.use("/login", login);
 
+app.use(currentUser);
+app.use(requireAuth);
+
+// this function will work only when the cookie named 'auth' with correct jwt value is passed in
+app.get("/testt", (req, res) => {
+    res.status(200).send("hola");
+});
 
 app.use(ErrorHandler);
 
 export default app;
-
