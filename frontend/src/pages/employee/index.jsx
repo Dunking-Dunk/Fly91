acf7bcbd7e5ae49d2dae4701600bb45524a43429
sam-verse <220801009@rajalakshmi.edu.in>
2024-09-28 11:18:57 +0530

    added service detail page

--- frontend/src/pages/employee/index.jsx
+++ frontend/src/pages/employee/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Dasboard from './Dasboard'
-import All from './All'
+import Bookformy from './forms/passenger'
 
 const index = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Dasboard/>}/>
-        <Route path='/all' element={<All/>}/>
+        <Route path='/details' element={<Bookformy/>}/>
       </Routes>
     </div>
   )
