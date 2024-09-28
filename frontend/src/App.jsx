749ae48197b20cc92109f7388934ce303b4fab01
derickdilip <derickdilip05@gmail.com>
2024-09-28 11:25:59 +0530

    added review

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Login from './pages/login'
 import Main from './pages/main'
+import Rflight from './pages/employee/review/flight'
+import Rhotel from './pages/employee/review/hotel'
+import Rcab from './pages/employee/review/cab'
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/login/*' element={<Login />} />
         <Route path='/*' element={<Main />} />
+        <Route path='/rflight' element={<Rflight />} />
+        <Route path='/rhotel' element={<Rhotel />} />
+        <Route path='/rcab' element={<Rcab />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/employee/review/flight.jsx
@@ -0,0 +1,212 @@
+import React, { useState } from 'react';
+import { ChevronDown, Hotel, Car } from 'lucide-react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+// Define Zod schemas
+const passengerSchema = z.object({
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+});
+
+const serviceSchema = z.object({
+  origin: z.string().min(1, "Origin is required"),
+  departure: z.string().min(1, "Departure is required"),
+  departureDate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format"),
+  arrivalDate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format"),
+  mobileNumber: z.string().regex(/^\d{10}$/, "Invalid mobile number"),
+});
+
+const formSchema = z.object({
+  passengers: z.array(passengerSchema).length(3, "Exactly 3 passengers are required"),
+  service: serviceSchema,
+});
+
+export default function FlightBooking() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isPassengerEditMode, setIsPassengerEditMode] = useState(false);
+  const [isServiceEditMode, setIsServiceEditMode] = useState(false);
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      passengers: [{}, {}, {}],
+      service: {},
+    },
+  });
+
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // Handle form submission
+  };
+
+  return (
+    <div className="flex justify-between items-center mb-6 border border-gray-200 rounded-lg p-2 bg-[#E4F6FD]">
+      <div className="flex-1 p-5">
+        <div className="bg-white py-10 px-10 rounded-lg shadow-sm">
+          {/* Top layer */}
+          <div className="flex justify-between items-center mb-6, border border-gray-200 rounded-lg p-4 mb-2 bg-[#fafafa]">
+            <h2 className="text-md font-normal">Review Passenger Details</h2>
+            <div className="flex justify-between items-center mb-6, border border-gray-200 rounded-lg p-2 bg-[#E4F6FD]">
+              <img src="https://cdn-icons-png.flaticon.com/512/74/74510.png" className="h-5 w-5 text-gray-500 mr-2" viewBox="0 0 20 20" fill="currentColor" />
+                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+              <span className="text-sm text-gray-500">Flight</span>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* Passenger Details Box */}
+            <div className="border border-gray-200 rounded-lg p-4 mb- bg-[#fafafa]">
+              <div className="flex justify-between items-center mb-4">
+                <div className="flex items-center justify-end w-full">
+                  <input 
+                    type="checkbox" 
+                    id="edit" 
+                    className="mr-2"
+                    checked={isPassengerEditMode}
+                    onChange={() => setIsPassengerEditMode(!isPassengerEditMode)}
+                  />
+                  <label htmlFor="edit" className="text-sm text-gray-500">Edit</label>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                {[0, 1, 2].map((index) => (
+                  <React.Fragment key={index}>
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="First Name"
+                        className="w-full p-2 border border-gray-300 rounded p-5"
+                        disabled={!isPassengerEditMode}
+                        {...register(`passengers.${index}.firstName`)}
+                      />
+                      {errors.passengers?.[index]?.firstName && (
+                        <p className="text-red-500 text-sm mt-1">{errors.passengers[index].firstName.message}</p>
+                      )}
+                    </div>
+                  </React.Fragment>
+                ))}
+                {[0, 1].map((index) => (
+                  <React.Fragment key={index}>
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="Last Name"
+                        className="w-full p-2 border border-gray-300 rounded p-5"
+                        disabled={!isPassengerEditMode}
+                        {...register(`passengers.${index}.lastName`)}
+                      />
+                      {errors.passengers?.[index]?.lastName && (
+                        <p className="text-red-500 text-sm mt-1">{errors.passengers[index].lastName.message}</p>
+                      )}
+                    </div>
+                  </React.Fragment>
+                ))}
+                <div className="border border-gray-200 rounded-lg p-5 bg-[#ffffff]">
+                  <span className="text-sm font-medium text-gray-500">ONE WAY</span>
+                </div>
+              </div>
+            </div>
+
+            {/* Service Request Box */}
+            <div className="flex justify-between items-center mb-2 border border-gray-200 rounded-lg p-4 mb-2 bg-[#fafafa]">
+              <h3 className="text-md font-normal">Service Request</h3>
+            </div>
+            <div className="border border-gray-200 rounded-lg p-4 bg-[#fafafa]">
+              <div className="flex justify-between items-center mb-4">
+                <div className="flex items-center justify-end w-full">
+                  <input 
+                    type="checkbox" 
+                    id="edit-service" 
+                    className="mr-2"
+                    checked={isServiceEditMode}
+                    onChange={() => setIsServiceEditMode(!isServiceEditMode)}
+                  />
+                  <label htmlFor="edit-service" className="text-sm text-gray-500">Edit</label>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Origin"
+                    className="w-full p-2 border border-gray-300 rounded p-5 bg-[#F4F4F3]"
+                    disabled={!isServiceEditMode}
+                    {...register('service.origin')}
+                  />
+                  {errors.service?.origin && (
+                    <p className="text-red-500 text-sm mt-1">{errors.service.origin.message}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Departure"
+                    className="w-full p-2 border border-gray-300 rounded p-5 bg-[#F4F4F3]"
+                    disabled={!isServiceEditMode}
+                    {...register('service.departure')}
+                  />
+                  {errors.service?.departure && (
+                    <p className="text-red-500 text-sm mt-1">{errors.service.departure.message}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type="date"
+                    placeholder="Departure Date"
+                    className="w-full p-2 border border-gray-300 rounded p-5 bg-[#F4F4F3]"
+                    disabled={!isServiceEditMode}
+                    {...register('service.departureDate')}
+                  />
+                  {errors.service?.departureDate && (
+                    <p className="text-red-500 text-sm mt-1">{errors.service.departureDate.message}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type="date"
+                    placeholder="Arrival Date"
+                    className="w-full p-2 border border-gray-300 rounded p-5 bg-[#F4F4F3]"
+                    disabled={!isServiceEditMode}
+                    {...register('service.arrivalDate')}
+                  />
+                  {errors.service?.arrivalDate && (
+                    <p className="text-red-500 text-sm mt-1">{errors.service.arrivalDate.message}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Mobile Number"
+                    className="w-full p-2 border border-gray-300 rounded p-5 bg-[#F4F4F3]"
+                    disabled={!isServiceEditMode}
+                    {...register('service.mobileNumber')}
+                  />
+                  {errors.service?.mobileNumber && (
+                    <p className="text-red-500 text-sm mt-1">{errors.service.mobileNumber.message}</p>
+                  )}
+                </div>
+                <div className="border border-gray-200 rounded-lg p-5 bg-[#F4F4F3]">
+                  <span className="text-sm font-medium text-gray-500">ONE WAY</span>
+                </div>
+              </div>
+            </div>
+
+            {/* Action Buttons */}
+            <div className="flex justify-end space-x-4 mt-6">
+              <button type="button" className="w-48 bg-yellow-500 hover:bg-yellow-600 text-white py-4 px-4 rounded">
+                Back
+              </button>
+              <button type="submit" className="w-48 bg-green-500 hover:bg-green-600 text-white py-4 px-4 rounded">
+                Place Request
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
