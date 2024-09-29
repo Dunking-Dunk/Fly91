9a9929ac817361e02be6907fb7151d5e05b6dcc3
dxxpx <dxxpka.20@gmail.com>
2024-09-29 14:44:20 +0530

    FEAT - add expressjs

--- backend-v2/src/business_logic/service_requests.js
+++ backend-v2/src/business_logic/service_requests.js
@@ -1,4 +1,11 @@
-async function createServiceRequestDataHandler(
+import dotenv from 'dotenv';
+
+// Assuming you have prismaClient.js
+
+// Load environment variables
+dotenv.config();
+
+export async function createServiceRequestDataHandler(
     serviceRequestData,
     serviceType,
     additionalDetails,
@@ -11,7 +18,6 @@ async function createServiceRequestDataHandler(
         {
             serviceRequestID,
             ...restData,
-            // ...additionalDetails,
             status: 'Submitted',
             statusTimestamp: new Date(),
             createdAt: new Date(),
@@ -43,82 +49,94 @@ async function createServiceRequestDataHandler(
     return { serviceRequestID };
 }
 
-async function AddrequestToServiceType(serviceTable, data, prisma) {
+export async function AddrequestToServiceType(serviceTable, data, prisma) {
     await createRecord(serviceTable, data, prisma);
     console.log(`Added to ${serviceTable} request`);
 }
 
-function generateSRID(requestType) {
+export function generateSRID(requestType) {
     const upperCaseRequestType = requestType.toUpperCase();
     const randomSixDigitNumber = Math.floor(100000 + Math.random() * 900000);
     return `SRN-${upperCaseRequestType}-${randomSixDigitNumber}`;
 }
 
 export const createRecord = async (modelName, data, prisma) => {
-    // try {
     return await prisma[modelName].create({ data });
-    // } catch (error) {
-    //     console.error(`Error creating record in ${modelName}:`, error);
-    //     throw new Error(`Failed to create record in ${modelName}`);
-    // }
 };
 
-const reqBody = {
-    serviceType: 'Cab',
-    firstName: 'Aarav',
-    lastName: 'Sharma',
-    employeeID: 'FLY91-0002',
-    department: 'Flight Ops',
-    mobileNumber: '9876543210',
-    email: 'aarav.sharma@example.com',
-    reasonForTravel: 'Sales_Travel',
-    hodApprovalAttachment: null,
-    additionalDetails: {
-        requestType: 'Cab',
-        city: 'London',
-        state: 'Paris',
-        pickUpAddress: '3rd street',
-        dropAddress: '9th street',
-        dateOfTravel: '2024-10-10T10:00:00.000Z',
-        startTime: '2024-10-10T10:00:00.000Z',
-        endTime: '2024-10-10T10:00:00.000Z',
-        pickUpTime: '2024-10-10T10:00:00.000Z',
-        createdAt: '2024-10-10T10:00:00.000Z',
-    },
-};
+//CAB REQUEST
+// const reqBody = {
+//     serviceType: 'Cab',
+//     firstName: 'Aarav',
+//     lastName: 'Sharma',
+//     employeeID: 'FLY91-0002',
+//     department: 'Flight Ops',
+//     mobileNumber: '9876543210',
+//     email: 'aarav.sharma@example.com',
+//     reasonForTravel: 'Sales_Travel',
+//     hodApprovalAttachment: null,
+//     additionalDetails: {
+//         requestType: 'Cab',
+//         city: 'London',
+//         state: 'Paris',
+//         pickUpAddress: '3rd street',
+//         dropAddress: '9th street',
+//         dateOfTravel: '2024-10-10T10:00:00.000Z',
+//         startTime: '2024-10-10T10:00:00.000Z',
+//         endTime: '2024-10-10T10:00:00.000Z',
+//         pickUpTime: '2024-10-10T10:00:00.000Z',
+//         createdAt: '2024-10-10T10:00:00.000Z',
+//     },
+// };
 
-const {
-    employeeID,
-    firstName,
-    lastName,
-    department,
-    mobileNumber,
-    email,
-    reasonForTravel,
-    hodApprovalAttachment,
-    serviceType,
-    additionalDetails,
-} = reqBody;
+//HOTEL BODY
+// const reqBody = {
+//     serviceType: "Hotel",
+//     firstName: "Aarav",
+//     lastName: "Sharma",
+//     employeeID: "FLY91-0002",
+//     department: "Flight Ops",
+//     mobileNumber: "9876543210",
+//     email: "aarav.sharma@example.com",
+//     reasonForTravel: "Sales_Travel",
+//     hodApprovalAttachment: null,
+//     additionalDetails: {
+//         city: "Chennai",
+//         state: "TN",
+//         checkInDate: "2024-10-05T14:00:00.000Z",
+//         checkInTime: "2024-10-05T10:00:00.000Z",
+//         checkOutDate: "2024-10-10T12:00:00.000Z",
+//         checkOutTime: "2024-10-10T10:00:00.000Z",
+//         createdAt: "2024-10-10T10:00:00.000Z"
+//     }
+// }
 
-const fullname = `${firstName} ${lastName}`;
-const serviceRequestID = generateSRID(serviceType);
-const serviceRequestData = {
-    serviceRequestID,
-    employeeID,
-    name: fullname,
-    department,
-    mobileNumber,
-    reasonForTravel,
-    hodApprovalAttachment,
-    serviceType,
-};
-
-import 'dotenv';
-import prisma from '../db/prismaClient.js';
-
-createServiceRequestDataHandler(
-    serviceRequestData,
-    serviceType,
-    additionalDetails,
-    prisma,
-);
+//FLIGHT BODY
+// const reqBody = {
+//     serviceType: "Flight",
+//     firstName: "Aarav",
+//     lastName: "Sharma",
+//     employeeID: "FLY91-0001",
+//     department: "Flight Ops",
+//     mobileNumber: "9876543210",
+//     email: "aarav.sharma@example.com",
+//     reasonForTravel: "Sales_Travel",
+//     hodApprovalAttachment: null,
+//     additionalDetails: {
+//         tripType: "Multi_City",
+//         origin: [
+//             "New York",
+//             "London"
+//         ],
+//         destination: [
+//             "London",
+//             "India"
+//         ],
+//         departureDate: "2024-10-05T14:00:00.000Z",
+//         arrivalDate: "2024-10-10T12:00:00.000Z",
+//         departureTimePreference: null,
+//         arrivalTimePreference: null,
+//         createdAt: "2024-09-28T14:00:00.000Z",
+//         class: "Business"
+//     }
+// }
