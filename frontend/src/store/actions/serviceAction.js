3e7fbed5d6f28c6313e9f8ed419f8f7c1aaf7959
AKSTHIRUMURUGAN <nourishnetofficial@gmail.com>
2024-09-29 12:57:09 +0530

    service action modified

--- frontend/src/store/actions/serviceAction.js
+++ frontend/src/store/actions/serviceAction.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { deleteServiceFail, deleteServiceRequest, deleteServiceSuccess, myServicesFail, myServicesRequest, myServicesSuccess, serviceFail, serviceRequest, servicesFail, servicesRequest, servicesSuccess, serviceSuccess, updateServiceFail, updateServiceRequest, updateServiceSuccess } from "../slices/serviceSlice"
+import { createServiceFail, createServiceRequest, createServiceSuccess, deleteServiceFail, deleteServiceRequest, deleteServiceSuccess, myServicesFail, myServicesRequest, myServicesSuccess, serviceFail, serviceRequest, servicesFail, servicesRequest, servicesSuccess, serviceSuccess, updateServiceFail, updateServiceRequest, updateServiceSuccess } from "../slices/serviceSlice"
 export const getAllRequestes=()=>async(dispatch)=>{
 try {
     dispatch(servicesRequest())
@@ -18,11 +18,19 @@ export const getMyRequestes=()=>async(dispatch)=>{
         dispatch(myServicesFail(error.response.data.message))
     }
 }
-
-export const getRequest=()=>async(dispatch)=>{
+export const createRequest=()=>async(dispatch)=>{
+    try {
+       dispatch(createServiceRequest())
+       axios.delete(`/employee/request/${id}`)
+        dispatch(createServiceSuccess())
+    } catch (error) {
+        dispatch(createServiceFail(error.response.data.message))
+    }
+}
+export const getRequest=(id)=>async(dispatch)=>{
     try {
         dispatch(serviceRequest())
-        const services=axios.get("/myservices")
+        const services=axios.get(`/employee/request/${id}`)
         dispatch(serviceSuccess(services))
     } catch (error) {
         dispatch(serviceFail(error.response.data.message))
@@ -32,7 +40,7 @@ export const getRequest=()=>async(dispatch)=>{
 export const updateRequest=()=>async(dispatch)=>{
     try {
         dispatch(updateServiceRequest())
-        const services=axios.push("/myservices")
+        const services=axios.push(`/employee/request/${id}`)
         dispatch(updateServiceSuccess(services))
     } catch (error) {
         dispatch(updateServiceFail(error.response.data.message))
@@ -41,7 +49,7 @@ export const updateRequest=()=>async(dispatch)=>{
 export const deleteRequest=()=>async(dispatch)=>{
     try {
         dispatch(deleteServiceRequest())
-       axios.delete("/myservices")
+       axios.delete(`/employee/request/${id}`)
         dispatch(deleteServiceSuccess())
     } catch (error) {
         dispatch(deleteServiceFail(error.response.data.message))
